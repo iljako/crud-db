4f73a71fc548462f45f514630e6b4270f0966969
iljako <iljakokorev@gmail.com>
2026-06-01 21:51:07 +0300

    Add Singleton JdbcUtils

---
+++ src/main/java/com/ilja/cruddb/utils/JdbcUtils.java
@@ -0,0 +1,29 @@
+package com.ilja.cruddb.utils;
+
+import com.ilja.cruddb.config.DatabaseConfig;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcUtils {
+    private static volatile JdbcUtils instance;
+    private static Connection connection = null;
+
+    private JdbcUtils() {
+        try {
+            connection = DatabaseConfig.getDataSource().getConnection();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static PreparedStatement getPreparedStatement(String sql) throws SQLException {
+        if (instance == null) {
+            instance = new JdbcUtils();
+        }
+        return connection.prepareStatement(sql);
+    }
+}
